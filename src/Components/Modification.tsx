3c01faf077dd7045963511401481f5cc06febeef
KamehaMehdi <dahmammehdi@gmail.com>
2019-07-04 14:57:02 +0200

    Start workin on modifier component

---
+++ src/Components/Modification.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react'
+import { Card, Button, Input, DatePicker, TimePicker } from 'antd';
+import { DiligencesInterface } from '../utils/interfaces';
+import moment from 'moment'
+import { makeStyles } from '@material-ui/styles'
+import { Lazy } from '../utils/Lazy';
+import { Get } from '../utils/api';
+const { useState } = React
+interface props {
+    datas: DiligencesInterface
+}
+
+export const Modification = (props:props) => {
+    return <DiligenceForm datas={props.datas} />
+    // return <Lazy promise={Get('diligences')} Component={DiligenceForm} />
+}
+
+const useStyle = makeStyles({
+    truc:{
+        height:'100%'
+    }
+})
+
+const DiligenceForm = (props:props) => {
+
+    const classe = useStyle()
+
+    const { datas } = props;
+    const { Collaborateur, DateCourses, Detail, Dossier, Heure_TotalDecimal } = datas;
+    const [collaborateur, setCollaborateur] = useState(Collaborateur)
+    const [dateCourses, setDateCourses] = useState(DateCourses.split('T')[0])
+    const [detail, setDetail] = useState(Detail)
+    const [dossier, setDossier] = useState(Dossier)
+    const [heureTotal] = useState(Heure_TotalDecimal.toString().split('.')[0])
+    const [minuteTotal] = useState(Heure_TotalDecimal.toString().split('.')[1])
+    const [tempsTotal, setTempsTotal] = useState()
+
+    const [allData, setAllData] = useState(datas)
+
+    return<Card className={classe.truc} >
+            <div style={{display: 'flex', flexDirection: 'row'}} >
+                <label htmlFor='date'>Date: </label>
+                <DatePicker id='date' name='date' defaultValue={moment(dateCourses, 'YYYY-MM-DD')} onChange={(el:any) => setDateCourses(el.target.value)} placeholder='Date'/>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row'}} >
+                <label htmlFor="dossier">Dossier: </label>
+                <Input type='text' value={dossier} onChange={(el:any) => setDossier(el.target.value)} placeholder='Dossier' />
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row'}} >
+                <label htmlFor="collaborateur">Collaborateur: </label>
+                <Input type='text' value={collaborateur} onChange={(el:any) => setCollaborateur(el.target.value)} placeholder='Collaborateur'/>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row'}} >
+                <label htmlFor="heure">Temps passé: </label>
+                <TimePicker format='HH:mm' defaultValue={moment(`${heureTotal}:${minuteTotal}`,'HH:mm')} onChange={e => setTempsTotal(`${e.hour()},${e.minute()}`)}/>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row'}} >
+                <label htmlFor="detail">Details: </label>
+                <Input.TextArea value={detail} onChange={(el:any) => setDetail(el.target.value)} placeholder='Detail' />
+            </div>
+            <Button onClick={() => setAllData({Collaborateur:collaborateur, DateCourses: dateCourses, Detail: detail, Dossier: dossier, Heure_TotalDecimal: tempsTotal,ID: props.datas.ID})} >Valider</Button>
+    </Card>
+}
--- stories/index.story.js
+++ stories/index.story.js
@@ -1,5 +1,6 @@
 import 'antd/dist/antd.css'
 import { Domaines, Collaborateurs, Diligences, Dossiers, Clients } from './Get.story'
+import { Modification } from '../src/Components/Modification'
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { LoginStory } from './Login.story';
@@ -10,9 +11,21 @@ storiesOf('Kavoa', module)
 storiesOf('Login View', module)
     .add('Not connected', () => <LoginStory />)
 
+const diligences = {
+    Collaborateur: 38,
+    DateCourses: "2019-04-30T22:00:00.000Z",
+    Detail: "Finalisation mémo.",
+    Dossier: 16188,
+    Heure_TotalDecimal: 3.23,
+    ID: 154358
+}
+
+storiesOf('Modification View', module)
+    .add('No data', () => <Modification datas={diligences} />)
+
 storiesOf('HTTP GET', module)
     .add('domaines', () => <Domaines/>)
     .add('dossiers', () => <Dossiers/>)
-    .add('diligences', () => <Diligences/>)
+    .add('diligences', () => <Diligences />)
     .add('collaborateurs', () => <Collaborateurs/>)
     .add('clients', () => <Clients/>)
