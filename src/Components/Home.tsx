2f23166b0fd7f1a80d853857fca79c1bf4adeb2c
KamehaMehdi <dahmammehdi@gmail.com>
2019-07-12 11:32:47 +0200

    Adding home v1
    Signed-off-by: KamehaMehdi <dahmammehdi@gmail.com>

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 
 import { Card } from 'antd'
 
+import { Lazy } from '../utils/Lazy'
+import { TableAll } from '../Components/Table';
+
+import { Get } from '../utils/api';
+
 const valueOf = (table:Array<string>, search:string ) => table[ table.indexOf(search) + 1 ]
 const cookieTable = document.cookie.split(';').toString().split('=').toString().split(',').map((table:string) => table.trim())
 console.log(cookieTable)
@@ -13,12 +18,29 @@ export const HomePage = () => {
             <div style={{display:'flex', flexDirection:'row', flexWrap:'wrap'}}>
                 <Card>
                     Diligence
+                    <Lazy otherProps={{Type:'diligences'}} Component={Tableau} promise={Get("diligences")} />
                 </Card>
                 <Card>
                     Dossiers
+                    <Lazy otherProps={{Type:'dossiers'}} Component={Tableau} promise={Get('dossiers')} />
+                </Card>
+                <Card>
+                    Clients
+                    <Lazy otherProps={{Type:'clients'}} Component={Tableau} promise={Get('clients')} />
                 </Card>
                 </div>
             </div>
         </div>
+    )
+}
+
+const Tableau = ({datas,otherProps}:any) => {
+    const data = [];
+    for (let i = 0; i < 5; i++){
+        data.push(datas[i]);
+    }
+    console.log(data);
+    return (
+        <TableAll datas={data} otherProps={otherProps} />
     )
 }
